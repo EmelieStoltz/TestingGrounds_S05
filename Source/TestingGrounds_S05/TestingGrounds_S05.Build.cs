b6589bf8e45716c7f6cd2bffdcb8fa30621f6f78
EMELIE <emelie_michelle@hotmail.com>
2017-03-01 10:14:04 +0100

    Initial commit

---
+++ Source/TestingGrounds_S05/TestingGrounds_S05.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TestingGrounds_S05 : ModuleRules
+{
+	public TestingGrounds_S05(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/TestingGrounds_S05/TestingGrounds_S05.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __TESTINGGROUNDS_S05_H__
+#define __TESTINGGROUNDS_S05_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
