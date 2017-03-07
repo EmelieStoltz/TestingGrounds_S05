1f179a8cf35a7c9ec7aa47c73dfb37e230eeb93f
EMELIE <emelie_michelle@hotmail.com>
2017-03-07 17:48:02 +0100

    Testing Grounds - added new animations

--- Source/TestingGrounds_S05/TestingGrounds_S05GameMode.cpp
+++ Source/TestingGrounds_S05/TestingGrounds_S05GameMode.cpp
@@ -3,13 +3,13 @@
 #include "TestingGrounds_S05.h"
 #include "TestingGrounds_S05GameMode.h"
 #include "TestingGrounds_S05HUD.h"
-#include "TestingGrounds_S05Character.h"
+#include "Player/FirstPersonCharacter.h"
 
 ATestingGrounds_S05GameMode::ATestingGrounds_S05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/TestingGrounds_S05/TestingGrounds_S05HUD.cpp
+++ Source/TestingGrounds_S05/TestingGrounds_S05HUD.cpp
@@ -9,7 +9,7 @@
 ATestingGrounds_S05HUD::ATestingGrounds_S05HUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair.FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 
